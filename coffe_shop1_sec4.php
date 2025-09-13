8ef81b8a1d970e697e67c5ac2d4cbcb4f4c1b80b
nadinmufidaaa <nadinmufida@gmail.com>
2025-09-13 21:11:14 +0800

    nadin mufida

---
+++ coffe_shop1_sec4.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body style="padding:70px; font-family:helvetica; sans-serif">
+    <div style="font-size:14px;"> Tagline</div>
+   
+    <div style="margin-top:10px;font-weight:bold;font-size:28px;"> Contact us</div>
+
+    <div style="font-size:14px;margin-top:7px;">Lorem ipsum dolor sit amet, consector adipiscing elit.</div>
+
+    <div class="card-flex"style="display:flex; height: 600px; margin-top: 30px">
+        <div style="width:30%;">
+             <i class='bx bx-envelope' style="font-size:30px"></i>
+            <div style="font-weight:bold; margin-top:7px"> Email
+                <p style="font-size:12px; font-weight:normal">Lorem ipsum dolor sit amet.</p>
+                <a href="#" style="font-weight:normal; font-size:14px; color:black">sejahtera.coffee@example.com</a> 
+            </div><br><br>
+            <i class='bx bx-phone-call' style="font-size:30px"></i>
+            <div style="font-weight:bold; margin-top:10px"> Telepon
+                <p style="font-size:12px; font-weight:normal">Lorem ipsum dolor sit amet.</p>
+                <a href="#" style="font-weight:normal; font-size:14px; color:black">+62 812 3456 7890</a> 
+            </div><br><br>
+            <i class='bx bx-map' style="font-size:30px"></i>
+            <div style="font-weight:bold; margin-top:10px"> Alamat
+                <p style="font-size:12px; font-weight:normal">Jl. Sejahtera No. 123, Jakarta</p>
+                <p style="font-weight:normal; font-size:14px; color:black; cursor: pointer; margin-top: 25px">Get Directions ></p> 
+            </div>
+        </div>
+        <div style="width:80%; display:flex">
+            <img src="gambar.png" style="width:100%; height:75%; object-fit:cover;">
+        </div>
+    </div>
+
+    
+</body>
+</html>
