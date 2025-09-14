62314195054868fdf5f693cf8b49fcc21e52320b
tegarpng <tegarbijanta@gmail.com>
2025-09-14 08:55:53 +0800

    Tegar

---
+++ rating.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rating Section</title>
+    <link rel="stylesheet" href="Asset/stylingrating.css">
+</head>
+<body>
+    <div class="bold" style="font-size:40px">Testimoni Pelanggan</div>
+    <p><p></p>Kami sangat puas dengan pengalaman di sini!</p>
+    <div class="card-flex">
+        <div class="kolom" style="font-size:17px; font-weight:bold"> <p style="font-size: 20px;">&#9733; &#9733; &#9733; &#9733; &#9733;</p> "Kopi terbaik yang pernah saya cobai!"
+            <div class="profil">
+                <img src="Andi.jpg">
+            </div>
+            <div class="fontprofil"> <a href="">Andi Prabowo</a> 
+                <p>Manajer, PT Kopi</p>
+            </div>
+        </div>
+        <div class="jarakkolom" ></div>
+        <div class="kolom" style="font-size:17px; font-weight:bold"> <p style="font-size: 20px;">&#9733; &#9733; &#9733; &#9733; &#9733;</p> "Suasana yang nyaman dan pelayanan yang ramah!"</p>
+            <div class="profil">
+                <img src="Siti.jpg">
+            </div>
+            <div class="fontprofil"> <a href="#">Siti Aminah</a> 
+                <p>Pemilik, Cafe Rindu</p>
+            </div>
+        </div>
+        <div class="jarakkolom" ></div>
+        <div class="kolom" style="font-size:17px; font-weight:bold"> <p style="font-size: 20px;">&#9733; &#9733; &#9733; &#9733; &#9733;</p> "Menu yang bervariasi dan selalu segar!"
+            <div class="profil">
+                <img src="Budi.jpg">
+            </div>
+            <div class="fontprofil"> <a href="#">Budi Santoso</a> 
+                <p>Konsultan, Foodie</p>
+            </div>
+        </div>
+    </div>
+    <div class="keterangan">
+        <div class="kiri2" style="font-family: brush script mt">Logo</div>
+        <a href="#" class="tengah2">Menu <span>Link <span>Link <span>Link </span></span></span></a>
+        <div class="kanan2">
+            <img src="social.jpg">
+        </div>
+    </div>
+    <div style="border-bottom: solid 1px"></div>
+    <div class="footer"> &copy 2025 NamaUsaha. All rights reversed
+        <span> <a href="#" >Kebijakan Privasi</a> <span> <a href="#">Syarat Layanan</a>
+        <span> <a href="#" >Pengaturan Cookies</a></span></span>
+    </div>
+</body>
+</html>
