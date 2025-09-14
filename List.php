12ef22fb34e03d4071cf12e9e6f1d7cc2078ddb5
tegarpng <tegarbijanta@gmail.com>
2025-09-14 08:43:25 +0800

    Mail

---
+++ List.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Menu Spesial Kopi dan Snack</title>
+    <link rel="stylesheet" href="Asset/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h2>Nikmati</h2>
+        <h1>Menu Spesial Kopi dan Snack</h1>
+        <h2>Kami</h2>
+        <p>Kami menyajikan berbagai pilihan kopi dan snack yang menggugah selera. Temukan cita rasa 
+            <br>yang sempurna untuk menemani hari Anda.</br></p>
+        
+        <div class="section">
+            <img src="https://img.icons8.com/ios-filled/50/000000/like--v1.png" alt="Heart Icon">
+            <h3>Kopi Pilihan Terbaik untuk Anda</h3>
+            <p>Rasakan kelezatan kopi yang diseduh dengan cinta.</p>
+        </div>
+        
+        <div class="section">
+            <img src="https://img.icons8.com/ios-filled/50/000000/cookie.png" alt="Cookie Icon">
+            <h3>Snack Lezat untuk Menemani Kopi Anda</h3>
+            <p>Snack kami sempurna untuk melelangi pengalaman ngopi Anda.</p>
+        </div>
+        
+        <div class="section">
+            <img src="https://img.icons8.com/ios-filled/50/000000/calendar.png" alt="Calendar Icon">
+            <h3>Pesan Sekarang dan Nikmati Diskon!</h3>
+            <p>Dapatkan penawaran spesial dengan melakukan pemesanan hari ini.</p>
+        </div>
+        
+        <p><a href="#">Lihat Menu ></a></p>
+    </div>
+</body>
+</html>
