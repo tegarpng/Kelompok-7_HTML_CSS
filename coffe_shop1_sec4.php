5275b39ff11942dd03e912e0af4e7308931f60ad
nadinmufidaaa <nadinmufida@gmail.com>
2025-09-16 15:17:16 +0700

    nadin mufida

---
+++ coffe_shop1_sec4.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="assets/bebas.css">
+</head>
+<body>
+    <div style="font-size:14px;"> Tagline</div>
+   
+    <div style="margin-top:10px;font-weight:bold;font-size:28px;"> Contact us</div>
+
+    <div style="font-size:14px;margin-top:7px;">Lorem ipsum dolor sit amet, consector adipiscing elit.</div>
+
+    <div class="card-flex">
+        <div style="width:20%;">
+             <i class='bx bx-envelope' style="font-size:30px"></i>
+            <div class="komponen"> Email
+                <p >Lorem ipsum dolor sit amet.</p>
+                <a href="#" >sejahtera.coffee@example.com</a> 
+            </div><br><br>
+            <i class='bx bx-phone-call' style="font-size:30px"></i>
+            <div class="komponen"> Telepon
+                <p >Lorem ipsum dolor sit amet.</p>
+                <a href="#" >+62 812 3456 7890</a> 
+            </div><br><br>
+            <i class='bx bx-map' style="font-size:30px"></i>
+            <div class="komponen"> Alamat
+                <p >Jl. Sejahtera No. 123, Jakarta</p>
+                <a style="cursor: pointer; margin-top: 25px">Get Directions ></a> 
+            </div>
+        </div>
+        <div class="kolom">
+            <img src="gambar.png">
+        </div>
+    </div>
+
+    
+</body>
+</html>
